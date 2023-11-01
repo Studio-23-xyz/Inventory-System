466bf845f517a32ffb8f5be13aadfec613115b94
Md. Nurusshafi Evan <76874563+N-Evan@users.noreply.github.com>
2023-11-01 18:34:34 +0600

    v1.2.9
    version bump

---
+++ Runtime/Data/Item.cs
@@ -0,0 +1,13 @@
+using System;
+using UnityEngine;
+
+namespace Studio23.SS2.InventorySystem.Data
+{
+    [CreateAssetMenu(fileName = "New Item", menuName = "Studio-23/Inventory System/New Item")]
+    [Serializable]
+    public class Item : ItemBase
+    {
+        public Sprite Icon;
+    }
+
+}
---
+++ Runtime/Data/ItemBase.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace Studio23.SS2.InventorySystem.Data
+{
+    public class ItemBase : ScriptableObject
+    {
+        public string Id;
+        public string Name;
+        public string Description;
+
+    }
+}
